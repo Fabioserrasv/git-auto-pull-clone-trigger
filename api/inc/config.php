7648d818b84ed5e2251aecf057f036fc0403e73d
Fabioserrasv <fabioserra.sv@gmail.com>
2022-10-07 11:08:47 -0300

    Retirando dados de teste da configuração

--- api/inc/config.php
+++ api/inc/config.php
@@ -1,8 +1,8 @@
 <?php
 
-define('REP_HOST', 'OnixSistemas'); # Nome do usuário host.
-define('REP_NAME', 'onix_aso'); # Nome do repositório.
+define('REP_HOST', ''); # Nome do usuário host.
+define('REP_NAME', ''); # Nome do repositório.
 define('REP_BRANCH', ''); # Nome da branch.
-define('LOCALFOLDER', PROJECT_ROOT_PATH . '/../aso'); # Nome da pasta que deseja clonar/pull.
-define('PAT', getenv('PAT_GIT') . "@"); # Caso o repositório seja publico, não é necessário pat.
-define('PASS', 'ugOMs6M81');# Senha de segurança da route.
+define('LOCALFOLDER', ''); # Nome da pasta que deseja clonar/pull.
+define('PAT', ''); # Caso o repositório seja publico, não é necessário pat.
+define('PASS', '');# Senha de segurança da route.
